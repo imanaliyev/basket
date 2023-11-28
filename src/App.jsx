107dcdcca7e6e3b7f08acbe978f86a1d13cc3243
imanaliyev <mi357yzg6@code.edu.az>
2023-11-29 03:04:53 +0400

    salamlar

--- src/App.jsx
+++ src/App.jsx
@@ -6,61 +6,107 @@ import useLocalStorage from "./hook/useLocalStorage";
 
 function App() {
   const [data, setData] = useState([])
-  const [basketInfo , setBasketInfo] = useState(localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")):null)
-  const [count, setCount] = useLocalStorage(0)
+  const [basketInfo, setBasketInfo] = useState(localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [])
+  
 
   useEffect(() => {
+    getFetch()
     localStorage.setItem('items', JSON.stringify(basketInfo));
   }, [basketInfo]);
-  
 
-  const getFetch =()=>{
+
+  const getFetch = () => {
     fetch("https://northwind.vercel.app/api/products")
-    .then(res => res.json())
-    .then(data=>{
-      setData(data)
-   
-    
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
 
-    })
+
+
+      })
   }
-  getFetch()
-  
-  
-const addBasket=(item)=>{
-  setBasketInfo([...basketInfo,item])
-  setCount(parseInt(count)+1)
-}
-  
+  const remove = (id) => {
+
+
+    setBasketInfo(basketInfo.filter((x) => x.id !== id))
+
+
+
+
+  }
+
+
+
+  const addBasket = (item) => {
+    const elementIndex = basketInfo.findIndex((x) => x.id === item.id)
+    console.log(elementIndex);
+    if (elementIndex !== -1) {
+      const newBasket = [...basketInfo]
+      newBasket[elementIndex].count++
+      setBasketInfo(newBasket)
+    }
+    else {
+      setBasketInfo([...basketInfo, { ...item, count: 1 }])
+    }
+
+
+
+
+  }
+  const setCountValue = (isAdd, item) => {
+    const elementIndex = basketInfo.findIndex((x) => x.id === item.id)
+    const newBasket = [...basketInfo]
+    if (isAdd) {
+      newBasket[elementIndex].count++
+      setBasketInfo(newBasket)
+
+    }
+    else {
+      if (newBasket[elementIndex].count > 0) {
+        newBasket[elementIndex].count--
+        setBasketInfo(newBasket)
+
+      }
+
+
+    }
+
+
+  }
+
+
 
   return (
     <div className="App">
       <h1>My basket</h1>
       <div className="basket">
         <h1>basket</h1>
-        {basketInfo.map((item)=>(
+        {basketInfo.map((item) => (
           <ul>
             <li>{item.id}</li>
             <li>{item.name}</li>
-            <div>count : {count}</div>
+            <div>count : {item.count}</div>
+            <button onClick={() => setCountValue(false, item)}>-</button>
+            <button onClick={() => setCountValue(true, item)}>+</button>
+            <button onClick={() => remove(item.id, item.count)}>remove</button>
           </ul>
         ))}
       </div>
-      
-      {data.map(x=><ul key={x.id}>
+
+      {data.map(x => <ul key={x.id}>
         <li>{x.id}</li>
         <li>{x.name}</li>
-        <button onClick={()=>addBasket(x)}>add to basket</button>
-       
+        <button onClick={() => addBasket(x)}>add to basket</button>
+
       </ul>)}
 
-      
-      
-      
-     
+
+
+
+
     </div>
   )
-  };
+};
 
 
 export default App;
